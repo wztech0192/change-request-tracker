9791fb2455a9073e1d5f0aca7744ffb13d825873
wei zheng <weijie0191@gmail.com>
2019-01-19 23:33:22 -0500

    create client and add adminlte

---
+++ client/src/main.js
@@ -0,0 +1,15 @@
+import { sync } from 'vuex-router-sync';
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store/index';
+
+Vue.config.productionTip = false;
+
+sync(store, router);
+
+new Vue({
+  router,
+  store,
+  render(h) { return h(App); },
+}).$mount('#app');

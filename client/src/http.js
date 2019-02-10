32140de6c7e84a0b8111f37be70f4866e5637eb6
wei zheng <weijie0191@gmail.com>
2019-02-10 17:24:43 -0500

    config

--- client/src/http.js
+++ client/src/http.js
@@ -3,7 +3,9 @@ import store from "./store";
 
 // Do http request, time out after 5 sec
 export default () => axios.create({
-  baseURL: "/api",
+  baseURL: process.env.NODE_ENV === 'production'
+    ? '/crt/api'
+    : '/api',
   timeout: 5000,
   headers: {
     Authorization: `Bearer ${store.state.authentication.token}`
